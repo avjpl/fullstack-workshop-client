c7ee3ec290b5a0ba6066f64b4ec5694a29a24fe0
Jake <dawkinsjh@gmail.com>
2018-09-28 10:48:41 -0400

    added testing notes

--- src/components/__tests__/Filter.js
+++ src/components/__tests__/Filter.js
@@ -1,3 +1,16 @@
+/**
+ * Testing React Components:
+ * - Make sure they render with minimal props
+ * - You can use snapshot tests on rendered markup to make sure no
+ *    accidental changes are made in the future
+ * - components that make queries can have network requests mocked, alllwing the
+ *    previous two tests to be run against it
+ * - components that make mutations can be tested in the same way as listed
+ *    above, with an addition: You can test run the mutation, and make
+ *    sure the UI and apollo cache get updated properly
+ *
+ * SEE MORE HERE: https://www.apollographql.com/docs/guides/testing-react-components.html
+ */
 import React from 'react';
 import Filter from '../Filter';
 import renderer from 'react-test-renderer';
