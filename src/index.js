6ba30c5ee53018b2c9b31f31d9e8dc8e0cb1f39a
James Baxley <james@apollographql.com>
2018-10-08 22:58:16 -0400

    point to operation registry deployed server

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
 const client = new ApolloClient({
-  uri: 'https://fullstack-workshop-server.glitch.me/graphql',
+  // uri: 'https://fullstack-workshop-server.glitch.me/graphql',
+  uri: "https://fullstack-server-ljfikjsiqw.now.sh/",
   request: operation => {
     operation.setContext(context => ({
       headers: {
         ...context.headers,
-        authorization: localStorage.getItem('token'),
+        authorization: localStorage.getItem("token"),
       },
     }));
   },
   clientState: {
     defaults: {
-      isLoggedIn: !!localStorage.getItem('token'),
+      isLoggedIn: !!localStorage.getItem("token"),
     },
   },
 });
@@ -27,5 +28,5 @@ ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
