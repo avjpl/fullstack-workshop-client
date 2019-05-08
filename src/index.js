36568cb3d6886441d44589c6f68a23a32cf602df
Peggy Rayzis <peggyrayzis@gmail.com>
2019-05-07 22:16:36 -0700

    Update server URI

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
 const client = new ApolloClient({
-  uri: 'https://fullstack-workshop-server.glitch.me/graphql',
+  uri: "https://6v1lkoqpy3.sse.codesandbox.io/",
   request: operation => {
     operation.setContext(context => ({
       headers: {
         ...context.headers,
-        authorization: localStorage.getItem('token'),
-      },
+        authorization: localStorage.getItem("token")
+      }
     }));
-  },
+  }
 });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
